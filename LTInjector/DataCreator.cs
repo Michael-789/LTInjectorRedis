73c540837a4f105b53b17fa77aa4a5b97fcefdde
Michael-789 <53147330+Michael-789@users.noreply.github.com>
2023-07-06 16:09:10 +0300

    new data creation

--- LTInjector/DataCreator.cs
+++ LTInjector/DataCreator.cs
@@ -12,18 +12,27 @@ namespace LTInjector
 {
     internal class DataCreator
     {
-        //static readonly double x = 0.5;
-        //static readonly double y = 1.5;
+       // static  double x = 100;
+        //static  double y = 100;
         static readonly double z = 1000;
 
         static readonly int speed = 850;
 
         static int id = 1;
+        
+        static int trackId = 1;
+
+        private static double xAdv = (Constants.XEND - Constants.XSTART) / Constants.TRACKPERIOD;
+        private static double yAdv = (Constants.YEND - Constants.YSTART) /Constants.TRACKPERIOD;
+        
+        private static double x = Constants.XSTART;
+        private static double y = Constants.YSTART;
+
 
         static int alertscounter = 0;
 
-        List<Point> intPoints;
-        List<Point> extPoints;
+        /*List<Point> intPoints;
+        List<Point> extPoints;*/
 
         ReddisHandler reddis;
 
@@ -32,8 +41,8 @@ namespace LTInjector
 
         public DataCreator()
         {
-            intPoints = createIntPoints();
-            extPoints = createExtPoints();
+            //intPoints = createIntPoints();
+           // extPoints = createExtPoints();
             reddis = new ReddisHandler();
         }
    /*     public void send2Rabbit()
@@ -97,7 +106,9 @@ namespace LTInjector
                 foreach(string body in arrayList)
                 {
                     listCount++;
-                    reddis.push2List("flightList",body);
+                    Console.WriteLine(body);
+                    reddis.send2Set("flightList",body);
+                    //reddis.push2List("flightList",body);
                    // rabbitMQSender.Send(body);
 
                 }
@@ -130,12 +141,10 @@ namespace LTInjector
             List<string> arrayList = new List<string>();
 
 
-            int intPointsIndex = 0;
-            int outPointsIndex = 0;
-            for (int i = 0; i < Constants.MSG_PER_SEC; i++)
+          for (int i = 0; i < Constants.MSG_PER_SEC; i++)
             {
-                double newX = 0;
-                double newY = 0;
+               // double newX = 0;
+               // double newY = 0;
                 double newZ = z;
 
 
@@ -145,44 +154,47 @@ namespace LTInjector
                     newZ = 1001;
                     alertscounter++;
                 }
-                else if ((id % 10) == 0)
+                /*else if ((id % 10) == 0)
                 {
-                    newX = 42;
-                    newY = 42;
+                    //newX = 42;
+                   // newY = 42;
                     alertscounter++;
 
                 }
-                else if ((id % 5) == 0)
-                {
+                else #1#*/
+              
                     
-                    Point p = intPoints[intPointsIndex];
-                    newX = p.X;
-                    newY = p.Y;
+                    //Point p = intPoints[intPointsIndex];
+                    //newX = p.X;
+                    //newY = p.Y;
 
-                    if (++intPointsIndex >= intPoints.Count )
+                    /*if (++intPointsIndex >= intPoints.Count )
                     {
                         intPointsIndex = 0;
-                    }
+                    }*/
+                 
                     alertscounter++;
 
-                }
-                else
-                {
+                
+               // else
+               // {
                   
+                    /*
                     Point p = extPoints[outPointsIndex];
                     newX = p.X;
                     newY = p.Y;
                     if (++outPointsIndex >= extPoints.Count)
                     {
                         outPointsIndex = 0;
-                    }
-                }
+                    }*/
+                //}
 
 
                 flightJson.Add("Id", id++);
+                flightJson.Add("trackId",trackId);
                 flightJson.Add("location", new JsonObject()
                   {   {"type", "Point"},
-                      { "coordinates", new JsonArray() { newX, newY } },
+                      { "coordinates", new JsonArray() { x, y } },
 
                   });
                 flightJson.Add("altitude", newZ);
@@ -194,12 +206,26 @@ namespace LTInjector
                 arrayList.Add(flightJson.ToString());
             }
 
+            if (trackId == Constants.TRACKPERIOD + 1)
+            {
+                trackId = 1;
+                x = Constants.XSTART;
+                y = Constants.YSTART;
+            }
+            else
+            {
+                trackId++;
+                x = Math.Round((x + xAdv),2);
+                y = Math.Round((y +yAdv),2);
+            }
+
+
             return arrayList;
 
 
         }
 
-        private List<Point> createIntPoints()
+        /*private List<Point> createIntPoints()
         {
             List<Point> points = new List<Point>();
 
@@ -209,9 +235,9 @@ namespace LTInjector
 
             return points;
 
-        }
+        }*/
 
-        private List<Point> createExtPoints()
+        /*private List<Point> createExtPoints()
         {
             List<Point> points = new List<Point>();
 
@@ -221,6 +247,6 @@ namespace LTInjector
             points.Add(new Point(1.5, 0.5));
 
             return points;
-        }
+        }*/
     }
 }
